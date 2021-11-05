cbdb498d42495aa79bd2ab74fcc159e395e1ac57
ALAM-97 <alexamara97@gmail.com>
2021-11-05 17:45:54 +0100

    Footer Completed

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,3 +1,82 @@
-<footer>
-    
-</footer>
+<div class="bg-footer">
+    <footer class="wrapper">
+    <div class="footer-left">
+        <div class="col">
+            <h6>DC COMICS</h6>
+            <ul>
+                <li>Characters</li>
+                <li>Comics</li>
+                <li>Movies</li>
+                <li>TV</li>
+                <li>Games</li>
+                <li>Videos</li>
+                <li>News</li>
+            </ul>
+            <h6>SHOP</h6>
+            <ul>
+                <li>Shop DC</li>
+                <li>Shop DC Collectibles</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h6>DC</h6>
+            <ul>
+                <li>Terms Of Use</li>
+                <li>Privacy Policy(New)</li>
+                <li>Ad Choices</li>
+                <li>Advertising</li>
+                <li>Jobs</li>
+                <li>Subscriptions</li>
+                <li>Talent Workshops</li>
+                <li>CPSC Certificates</li>
+                <li>Rating</li>
+                <li>Shop Help</li>
+                <li>Contact Us</li>
+            </ul>
+        </div>
+        <div class="col">
+            <h6>SITES</h6>
+            <ul>
+                <li>DC</li>
+                <li>MAD Magazine</li>
+                <li>DC Kids</li>
+                <li>DC Universe</li>
+                <li>DC Power Visa</li>
+            </ul>
+        </div>
+    </div>
+    <div class="footer-right">
+        <img src="{{asset("images/dc-logo-bg.png")}}" alt="Footer DC Logo">
+    </div>
+    </footer>
+</div>
+<div class="footer-bottom-bg">
+    <div class="wrapper footer-bottom">
+        <div class="footer-bottom-left">
+            <button>
+                SIGN-UP NOW!
+            </button>
+        </div>
+        <div class="footer-bottom-right">
+            <h5>FOLLOW US</h5>
+            <ul>
+                <li>
+                    <img src="{{asset("images/footer-facebook.png")}}" alt="">
+                </li>
+                <li>
+                    <img src="{{asset("images/footer-twitter.png")}}" alt="">
+                </li>
+                <li>
+                    <img src="{{asset("images/footer-youtube.png")}}" alt="">
+                </li>
+                <li>
+                    <img src="{{asset("images/footer-pinterest.png")}}" alt="">
+                </li>
+                <li>
+                    <img src="{{asset("images/footer-periscope.png")}}" alt="">
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
+
